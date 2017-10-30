c3e11d70688d5ec9d3b1e695b828a2bf4f361ca6
ga15 <manas.dash@in.tesco.com>
2017-10-30 07:10:16 +0530

    using math library

---
+++ 100DaysOFCoding/MathWithPython.py
@@ -0,0 +1,21 @@
+import math
+import cmath
+
+#constants
+
+print(math.pi)
+print(math .e)
+
+# complex number
+
+print(math.modf(5.4))
+
+print(math.sin(0))
+
+print(math.exp(5))
+print(math.pow(5,6))
+
+z = complex(2,4)
+print(z)
+
+print(math.gcd(30,90))
